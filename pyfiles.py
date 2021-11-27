f80463cbb3bc05448054da3cd897cb52d0a2fd12
DS-X1 <82737641+DS-X1@users.noreply.github.com>
2021-11-27 10:09:19 -0400

    made it a while loop for extra convenience B)

--- pyfiles.py
+++ pyfiles.py
@@ -7,23 +7,26 @@ def multiply(x, y):
 def divide(x, y):
 	return x / y
 
+
+
 print('Calculator v1.0.2')
+print('This is still rudimentary, but I plan on making this BIG B)')
+while True:
+	num1 = int(input('Enter Number: '))
+	op = int(input('Would you like to Add(1), Subtract(2), Multiply(3), Divide(4): '))
+	num2 = int(input('Enter second number: '))
 
-num1 = int(input('Enter Number: '))
-op = int(input('What would you like to do (Add/1, Subtract/2, multiply/3, divide/4): '))
-num2 = int(input('Enter second number: '))
 
+	def operator(x, y):
+		print(num1, x , num2,'= ',y(num1, num2))
 
-def operator(x, y):
-	print(num1, x , num2,'= ',y(num1, num2))
+	if op == 1:
+		operator(op, '+', add)
+	if op == 2:
+		operator(op, '-', subtract)
+	if op == 3:
+		operator(op, '*', multiply)
+	if op == 4:
+		operator(op, '/', divide)
 
-if op == 1:
-	operator(op, '+', add)
-if op == 2:
-	operator(op, '-', subtract)
-if op == 3:
-	operator(op, '*', multiply)
-if op == 4:
-	operator(op, '/', divide)
-
-input("")
+	input("")
