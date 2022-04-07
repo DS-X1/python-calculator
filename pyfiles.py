1587871d06d1c71b03000192f90a970ecc522fda
DS-X1 <kylejosrph@protonmail.com>
2022-04-07 15:32:12 -0400

    Allowed options for basic integer operations

--- pyfiles.py
+++ pyfiles.py
@@ -12,21 +12,27 @@ def divide(x, y):
 print('Calculator v1.0.3')
 print('This is still rudimentary, but I plan on making this BIG B)')
 while True:
-	num1 = int(input('Enter Number: '))
-	op = int(input('Would you like to Add(1), Subtract(2), Multiply(3), Divide(4): '))
-	num2 = int(input('Enter second number: '))
+
+	print("State option: ")
+	operation = int(input("1. Integers with basic operations"))
+
+	if operation == 1:
+
+		num1 = int(input('Enter Number: '))
+		op = int(input('Add(1), Subtract(2), Multiply(3), Divide(4): '))
+		num2 = int(input('Enter second number: '))
 
 
-	def operator(x, y):
-		print(num1, x , num2,'= ',y(num1, num2))
+		def operator(x, y):
+			print(num1, x , num2,'= ',y(num1, num2))
 
-	if op == 1:
-		operator(op, '+', add)
-	if op == 2:
-		operator(op, '-', subtract)
-	if op == 3:
-		operator(op, '*', multiply)
-	if op == 4:
-		operator(op, '/', divide)
+		if op == 1:
+			operator(op, '+', add)
+		if op == 2:
+			operator(op, '-', subtract)
+		if op == 3:
+			operator(op, '*', multiply)
+		if op == 4:
+			operator(op, '/', divide)
 
-	input("")
+		input("")
